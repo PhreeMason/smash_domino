6eafb9ee8f3cdc50e3d61bded8054a3cbd086f56
Phreemason <mr.omarmason@gmail.com>
2018-01-23 00:46:29 -0500

    added additional styling to battlepage

--- app/components/BattlePage.js
+++ app/components/BattlePage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Text } from 'native-base';
+import { View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon, Button } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import LinearGradient from 'react-native-linear-gradient';
 import { StyleSheet } from 'react-native';
+import { Image, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
   linearGradient: {
@@ -21,30 +22,64 @@ const styles = StyleSheet.create({
   },
 });
 
+const images = [{ text: 'Mexico', name: 'Spaniard', image: require('../assets/img/island.jpg') }]
+
 const BattlePage = () => {
   return (
     <Grid>
-      <Row size={2}>
-        <Col style={{ backgroundColor: '#00CE9F' }} size={1}></Col>
-        <Col style={{ backgroundColor: '#635DB7' }} size={3}>
+    <LinearGradient colors={['#FFD3A5', '#FD6585']} locations={[0.1, 1]} style={{ width: Dimensions.get('window').width }}>
+        <Row size={2}>
 
-        </Col>
-        <Col style={{ backgroundColor: '#00CE9F' }} size={1}></Col>
-      </Row>
-      <Row size={1}>
-        <Col size={1}></Col>
-        <Col size={3}>
-          <LinearGradient colors={['#e65c00', '#f9d423']} style={styles.linearGradient}>
-            <Text style={styles.buttonText}>
-              Play
+          <Col style={{}} size={1}></Col>
+          <Col size={3}>
+            <View>
+              <DeckSwiper
+                dataSource={images}
+                renderItem={item =>
+                  <Card style={{ elevation: 3 }}>
+                    <CardItem>
+                      <Thumbnail source={item.image} />
+                      <Body>
+                        <Text>{item.text}</Text>
+                        <Text note>NativeBase</Text>
+                      </Body>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image style={{ height: 300, flex: 1 }} source={item.image}/>
+                    </CardItem>
+                    <CardItem>
+                      <Icon name="heart" style={{ color: '#ED4A6A' }} />
+                      <Text>{item.name}</Text>
+                    </CardItem>
+                  </Card>
+                }
+              />
+            </View>
+          </Col>
+          <Col style={{}} size={1}></Col>
+        </Row>
+        <Row size={1}>
+          <Col size={1}></Col>
+          <Col size={3} style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <LinearGradient colors={['#e65c00', '#f9d423']} style={{
+              height: 150,
+              width: 250,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 25
+            }}>
+              <Button transparent full style={{ height: 150 }}>
+                <Text>
+                  Play
             </Text>
-          </LinearGradient>
-        </Col>
-        <Col size={1}></Col>
-      </Row>
-    </Grid>
+              </Button>
+            </LinearGradient>
+          </Col>
+          <Col size={1}></Col>
+        </Row>
+    </LinearGradient>
+      </Grid>
   );
 }
-
 
 export default BattlePage
